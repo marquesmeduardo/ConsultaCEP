c44b8a82a0d54aa93baf540f05ad22f00af62544
marquesmeduardo <marquesmeduardo@gmail.com>
2017-06-20 09:04:43 -0300

    Add files via upload

---
+++ ConsultaCEP/BuscaCEP.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsultaCEP {
+    using System.Net;
+    using System.Web;
+    using WebServiceCEP;
+    public class BuscaCEP {
+        
+        public enderecoERP ConsultaCEP(string cep) {
+            bool isLocal = HttpContext.Current.Request.IsLocal;
+            if (isLocal) {
+                WebProxy p = new WebProxy("http://192.168.100.1:8080", true);
+                GlobalProxySelection.Select = p;
+            }
+
+            enderecoERP resposta = new enderecoERP();
+            using (var ws = new AtendeClienteClient()) {
+                resposta = ws.consultaCEP(cep);
+            }
+            return resposta;
+        }
+    }
+}
