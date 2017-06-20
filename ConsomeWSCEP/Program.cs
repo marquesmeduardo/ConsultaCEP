4a48d21d7d654420538ced7ae4d56a21427c22ea
marquesmeduardo <marquesmeduardo@gmail.com>
2017-06-20 09:06:26 -0300

    Add files via upload

---
+++ ConsomeWSCEP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsomeWSCEP {
+    using System.Net;
+    using ConsultaCEP;
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("CEP: ");
+            var cep = Console.ReadLine();
+
+            var obj = new BuscaCEP();
+            ConsultaCEP.WebServiceCEP.enderecoERP endereco = obj.ConsultaCEP(cep);
+            string logradouro = endereco.end;
+            string bairro = endereco.bairro;
+            string cidade = endereco.cidade;
+            string uf = endereco.uf;
+
+            Console.WriteLine("Endereço: "+logradouro+"\nBairro: "+bairro+"\nCidade: "+cidade+"\nEstado: "+uf);
+            Console.ReadKey();
+        }
+    }
+}
